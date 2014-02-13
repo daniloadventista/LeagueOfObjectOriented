d7e2cb77490ac8742963eb822094664464aba460
Tati0 <tathy_diorio@hotmail.com>
2014-02-13 15:13:04 -0200

    Storyboard novo

---
+++ LOOL/LOOL/LOOLViewController2.m
@@ -0,0 +1,49 @@
+//
+//  LOOLViewController2.m
+//  LOOL
+//
+//  Created by Tatiana Mourão Diorio on 2/13/14.
+//  Copyright (c) 2014 danilo. All rights reserved.
+//
+
+#import "LOOLViewController2.h"
+
+@interface LOOLViewController2 ()
+
+@end
+
+@implementation LOOLViewController2
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
