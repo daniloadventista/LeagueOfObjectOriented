7d706480163b812818553dade304bf071ad07df6
danilo <danilo.adventista@gmail.com>
2014-02-13 15:13:37 -0200

    Persist pt1

---
+++ LOOL/LOOL/LOOLPesrsistModel.h
@@ -0,0 +1,13 @@
+//
+//  LOOLPesrsistModel.h
+//  LOOL
+//
+//  Created by Danilo Lira de Lima on 13/02/14.
+//  Copyright (c) 2014 danilo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LOOLPesrsistModel : NSObject
+
+@end
---
+++ LOOL/LOOL/LOOLPesrsistModel.m
@@ -0,0 +1,13 @@
+//
+//  LOOLPesrsistModel.m
+//  LOOL
+//
+//  Created by Danilo Lira de Lima on 13/02/14.
+//  Copyright (c) 2014 danilo. All rights reserved.
+//
+
+#import "LOOLPesrsistModel.h"
+
+@implementation LOOLPesrsistModel
+
+@end
